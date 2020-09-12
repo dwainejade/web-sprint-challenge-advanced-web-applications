5f5df47324d40cd7aa00d0442d5bb57ea8cd8ffd
dwainejade <dwainejade@gmail.com>
2020-09-11 21:28:19 -0400

    login working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-// import AxiosAuth from '../utils/AxiosAuth'
-import { useHistory, Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleChange = e => {
+    e.persist()
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const token = localStorage.getItem("token")
-  axios
-    .post("http://localhost:5000/api")
-    .then((res) => {
-      console.log(res);
-      localStorage.setItem("token", res);
-      useHistory.push("/protected");
-  })
-    .catch((err) => console.log(err.response));
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res);
+        useHistory.push("/protected");
+      })
+      .catch((err) => console.log("Wrong ass pass!", err.response));
+  }
 
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
+      <form onSubmit={login} className="form-group">
+        <label>Username</label>
+        <input
+          className="form-control"
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn m-3 btn-primary">Login</button>
+      </form>
     </>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
