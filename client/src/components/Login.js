8662c0718552c0b54f5f4f8842758ad5a3549d81
dwainejade <dwainejade@gmail.com>
2020-09-11 20:12:10 -0400

    started

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,24 @@
 import React from "react";
+import axios from "axios";
+// import AxiosAuth from '../utils/AxiosAuth'
+import { useHistory, Redirect } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const token = localStorage.getItem("token")
+  axios
+    .post("http://localhost:5000/api")
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("token", res);
+      useHistory.push("/protected");
+  })
+    .catch((err) => console.log(err.response));
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
     </>
   );
 };
